d7a2d24ba73cae2a14f1f33005b8c63f8be46902
Joshua Anastasakis <joshuaanastasakis@gmail.com>
2022-03-27 13:47:51 -0400

    Functioning Demo

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-let currentAdviceID = null;
-let currentAdviceText = null;
+function updateAdviceID(newID) {
+    let id = document.getElementById("advice-id");
+    id.innerHTML = "ADVICE #"+newID;
+}
 
-function getAdvice() {
+function updateAdviceText(newText) {
+    let text = document.getElementById("advice-text");
+    text.innerHTML = "\"" + newText + "\"";
+}
+
+function updateAdvice() {
+    console.log("in updateAdvice function")
+    const rand = Math.floor(Math.random() * 224);
+    let response = fetch(`https://api.adviceslip.com/advice/${rand}`)
+        .then(res => res.json())
+        .then(data => {
+            const slip = data.slip;
+            console.log(slip);
+            updateAdviceID(slip.id);
+            updateAdviceText(slip.advice);
+        })
+        .catch(e => {
+            console.log(e);
+        });
     return {id: 0, advice: "advice text"}
 }
 
-const advice = getAdvice();
-currentAdviceID = advice.id;
-currentAdviceText = advice.advice;
+// 224 advices found
 
-function updateAdviceID(newID) {
-    
-}
+updateAdvice();
+
+let button = document.querySelector(".dice-container");
+button.addEventListener('click', e => {
+    updateAdvice();
+})
