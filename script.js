6415d8586a23ba2b8c86b79ce25d28fb084ec54b
= <=>
2022-03-27 11:55:14 -0400

    Started formatting content

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentAdviceID = null;
+let currentAdviceText = null;
+
+function getAdvice() {
+    return {id: 0, advice: "advice text"}
+}
+
+const advice = getAdvice();
+currentAdviceID = advice.id;
+currentAdviceText = advice.advice;
+
+function updateAdviceID(newID) {
+    
+}
